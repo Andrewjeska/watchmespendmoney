690fb854e7b46a26f176ffce6ccaa0fe628f15ca
Michael <m.anderjaska@gmail.com>
2020-07-11 17:13:45 -0700

    check for $PORT

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,10 @@
 import cors from "cors";
 import express from "express";
 import apiRoutes from "./api";
-// const port = parseInt(process.env.PORT || "5000", 10);
-//TODO figure out how dokku can do this
-const port = 5000;
-const dev = process.env.NODE_ENV !== "production";
+const port = parseInt(process.env.PORT || "5000", 10);
 
 const app = express();
 
-//   app.use(bodyParser.json());
-//   app.use(
-//     session({
-//       secret: "super-secret-key",
-//       resave: false,
-//       saveUninitialized: false,
-//       cookie: { maxAge: 60000 },
-//     })
-//   );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
