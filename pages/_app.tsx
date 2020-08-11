5113316d81ae2c4cbe187449733df42d434c179b
Michael <m.anderjaska@gmail.com>
2020-08-10 20:55:43 -0700

    SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "semantic-ui-css/semantic.min.css";
 import "./styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Watch me spend money: Stop wasting money on stupid things</title>
+        <meta
+          name="description"
+          content="Share your expenses with your friends to hold yourself accountable to your spending goals"
+        ></meta>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
